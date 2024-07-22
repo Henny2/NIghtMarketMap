f216dca8a50d9e0fed60bceab99609f3e4f12efe
Henny2 <henrieke.baunack@web.de>
2024-07-21 19:11:03 -1000

    Initial Commit

---
+++ NightMarketMapApp/NightMarketMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightMarketMapAppApp.swift
+//  NightMarketMapApp
+//
+//  Created by Henrieke Baunack on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct NightMarketMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
